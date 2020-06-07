2b4a3a76d6cc9bf21ae8f82f86493c1145818177
Adriano <adriano.barbosa90@gmail.com>
2020-06-07 14:12:30 -0300

    initial commit

---
+++ server/src/database/connection.ts
@@ -0,0 +1,13 @@
+import knex from 'knex'
+import path from 'path'
+
+const connnection = knex(
+{
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'database.sqlite')
+    },
+    useNullAsDefault: true
+})
+
+export default connnection
